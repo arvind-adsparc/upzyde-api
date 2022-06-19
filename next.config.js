b9da8efb666b622890c0ef49592a3ea0de0f973d
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-19 20:09:50 +0530

    api rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://upzyde-new-gatsby.vercel.app/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
