3e15a1a9c1095a0c15355703589557b5fb92d9c5
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-20 18:38:24 +0530

    submssion api email change

--- models/Submission.js
+++ models/Submission.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const submissionSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: [true, "Please enter your email"],
     lowercase: true,
-    unique: false,
   },
   page: {
     type: String,
