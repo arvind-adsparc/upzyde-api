dc9c2d381dafcbe5319c15eb6d5b4708e388dd3e
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-20 16:33:51 +0530

    contact form submission

--- pages/contact-form/index.js
+++ pages/contact-form/index.js
@@ -1,12 +1,46 @@
 import { useState, useEffect } from "react";
-import { Typography, Spin } from "antd";
+import { Typography, Spin, Space, Table, Tag } from "antd";
+
 import Layout from "../../models/components/Layout/layout";
 
 const { Title } = Typography;
 
+const columns = [
+  {
+    title: "First Name",
+    dataIndex: "fname",
+    key: "fname",
+  },
+  {
+    title: "Last Name",
+    dataIndex: "lname",
+    key: "lname",
+  },
+  {
+    title: "Company",
+    dataIndex: "company",
+    key: "company",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Company Type",
+    dataIndex: "companyType",
+    key: "companyType",
+  },
+  {
+    title: "Message",
+    dataIndex: "message",
+    key: "message",
+  },
+];
+
 const ContactForm = () => {
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     setLoading(true);
@@ -15,9 +49,22 @@ const ContactForm = () => {
       try {
         const res = await fetch("/api/contact");
         const result = await res.json();
-        setLoading(false);
 
-        console.log(result);
+        const modifiedData = result.data.map((info) => {
+          return {
+            firstName: info.firstName,
+            lastName: info.lastName,
+            company: info.company,
+            email: info.email,
+            companyType: info.companyType,
+            message: info.message,
+          };
+        });
+
+        setData(modifiedData);
+
+        console.log("result", result);
+        setLoading(false);
       } catch (err) {
         console.log("err", err);
         setLoading(false);
@@ -33,6 +80,10 @@ const ContactForm = () => {
         <Spin size="large" spinning={loading}>
           <Title>Contact Form Submissions</Title>
         </Spin>
+
+        <div>
+          <Table dataSource={data} columns={columns} />
+        </div>
       </Layout>
     </div>
   );
