c5edf41d9e40978bb4b49470ab2b6bd74cd84db3
Arvind M <arvindm@Arvinds-MacBook-Pro.local>
2022-06-20 15:37:39 +0530

    addition of layout component

--- models/components/Layout/layout.jsx
+++ models/components/Layout/layout.jsx
@@ -1,9 +1,28 @@
+import Image from "next/image";
+
 const Layout = ({ children }) => {
   return (
     <div className="layout-container">
-      <div className="header">Header goes here</div>
-      {children}
-      <div className="footer">footer goes here</div>
+      <div className="header">
+        <div className="inner-container">
+          <div className="head-logo">upzyde</div>
+        </div>
+      </div>
+
+      <div className="page-content inner-container">{children}</div>
+
+      <footer className="footer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <span className="logo">
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </div>
   );
 };
